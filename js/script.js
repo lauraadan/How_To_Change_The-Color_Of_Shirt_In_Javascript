5a987fe7a2ad35c20e6734c0f3e9ecd9e39d2bcd
lauraadan <info@lauraadan.com>
2022-02-20 18:55:14 +0100

    Primera subida al repo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// CAMBIO COLOR CAMISETA
+
+// Todas las funciones pueden llevar parámetros y se ponen dentro de los paréntesis de la función. Son como variables pero sin estar definidas.
+
+
+function changeImage(a) {
+
+
+        // Opción 1: 
+        // var target = document.getElementById("target");
+        // target.src = a;
+        // definir la variable NO es obligatorio
+
+
+        // Opción 2
+        document.getElementById("target").src=a;
+    }
+
+
+
